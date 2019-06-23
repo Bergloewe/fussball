21e2f2d1e0ac878c87b74f49fd5554048473875e
CK-byte <ka1368-416@online.de>
2019-06-23 16:29:56 +0200

    Reader für Spielerbibliothek_vol1

--- Spieler_auslesenBS.py
+++ Spieler_auslesenBS.py
@@ -1,97 +1,48 @@
 import requests
+import pandas
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import time
 import csv
+import string
 
-liga = 'bundesliga'
-year=1970
-day=2
+# liest die namen in url aus, speichert Set, aus der Spielerbib https://www.fussballdaten.de/person/,
 
-"""
-class CrawledArticle():
-    def __init__(self, Team1Spieler1, Team1Spieler2, Team1Spieler3, Team1Spieler4, Team1Spieler5, Team1Spieler6,...):
-        self.Team1Spieler1 = Team1Spieler1
-        self.Team1Spieler2 = Team1Spieler2
-        self.Team1Spieler3 = Team1Spieler3
-        self.Team1Spieler4 = Team1Spieler4
-        self.Team1Spieler5 = Team1Spieler5
-        self.Team1Spieler6 = Team1Spieler6
-        self.Team1Spieler7 = Team1Spieler7
-        self.Team1Spieler8 = Team1Spieler8
-        self.Team1Spieler9 = Team1Spieler9
-        self.Team1Spieler10 = Team1Spieler10
-        self.Team1Spieler11 = Team1Spieler11
-        self.Team1Trainer = Team1Trainer
+# id = 0 durch id wird set unnötig
+Spielerbib = set()
 
-        self.Team2Spieler1 = Team2Spieler1
-        self.Team2Spieler2 = Team2Spieler2
-        self.Team2Spieler3 = Team2Spieler3
-        self.Team2Spieler4 = Team2Spieler4
-        self.Team2Spieler5 = Team2Spieler5
-        self.Team2Spieler6 = Team2Spieler6
-        self.Team2Spieler7 = Team2Spieler7
-        self.Team2Spieler8 = Team2Spieler8
-        self.Team2Spieler9 = Team2Spieler9
-        self.Team2Spieler10 = Team2Spieler10
-        self.Team2Spieler11 = Team2Spieler11
-        self.Team2Trainer = Team2Trainer
-"""
-
-
-class ArticleFetcher():
-    def fetch(self):
-
-        url = "https://www.fussballdaten.de/" + liga + "/" + str(year) + "/" + str(day)
-        Personen = []
-
+# um alle Spieler zu erfassen müssen beide Buchstaben Aa, Ab durchgeklickt werden
+for first_letter in string.ascii_lowercase:
+    for second_letter in string.ascii_lowercase:
 
         time.sleep(1)
-        html_ausgelesen = requests.get(url)
-        doc = BeautifulSoup(html_ausgelesen.text, "html.parser")
-        i = 1
+        url = "https://www.fussballdaten.de/person/?letter=" + first_letter + "&sl=" + first_letter + second_letter
+        doc = BeautifulSoup(requests.get(url).text, "html.parser")
 
-        while url != "":
+        # nicht vorhandende Namen anfangen zB Bc
+        try:
+            doc.select(".row.p20")[0].select("a")
+        except:
+            print('No name with ' + str(first_letter) + str(second_letter))
+            continue
 
-            # Jetzt wird der Spielreport "angeklickt"
+        print(first_letter + second_letter)
 
-            for spielbuttonhtml in doc.select(".spiel-button-row"):
-                spielreport_url = urljoin(url, spielbuttonhtml.select_one("a").attrs["href"]) + "aufstellung/"
+        SpielerSeite = doc.select(".row.p20")[0].select("a")
 
-                html_Reportausgelesen = requests.get(spielreport_url)
-                Spielreport = BeautifulSoup(html_Reportausgelesen.text, "lxml")
+        for i in range(0, len(SpielerSeite)):  # geht alle Spieler auf der Webseite von Xx durch
 
-                for Spieler in Spielreport.select(".text.lineup"):
-                    player.append(Spieler.text)
-                    player.append(Spieler.attrs["href"][8:-6])
-                    time.sleep(1)
+            link = SpielerSeite[i].attrs["href"][8:-1]
+            name = SpielerSeite[i].text
 
+            # print("Die bib sieht so aus " + str(Spielerbib))
+            # print("Der neue Eintrag sieht so aus " + str((id,name,link)))
 
+            # Die if Abfrage weil die neue id +1 immer ins set rein bringt
+            # if ((id,name,link) in Spielerbib) == False:
+            # id = id + 1
+            Spielerbib.add((name, link))
 
-
-
-
-
-
-                emoji = card.select_one(".emoji").text
-                content = card.select_one(".card-text").text
-                title = card.select(".card-title span")[1].text
-                image = urljoin(url, card.select_one("img").attrs["src"])
-
-                crawled = CrawledArticle(title, emojiemoji, content, image)
-                articles.append(crawled)
-
-            i = i + 1  # Schleifen durchlauf zähler
-
-            # Nächste Seite
-            test = doc.select(".navigation")
-            if test:
-
-                navigation = doc.select(".navigation")[0]
-                url = urljoin(url, navigation.select_one("a").attrs["href"])
-                r = requests.get(url)
-                doc = BeautifulSoup(r.text, "html.parser")
-            else:
-                print("finish")
-                return articles
-
+with open('Spielerbib.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter='; ', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerows(sorted(Spielerbib))
